f3f0fec4d2ddd16bf5adc13c779d689fc2aaec73
Nabilanurfajrina <nabilafajrina9898@gmail.com>
2018-04-10 20:21:30 +0700

    updatewelcome

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,11 +20,8 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
-	}
-
-	public function about()
-	{
-		$this->load->view('about');
+		$this->load->view('home');
 	}
 }
+
+?>
